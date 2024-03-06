ff82b3ee8d5bd84ac372fc8247cfeb61296fd637
hammad18100 <hammad.superchamp@gmail.com>
2024-03-06 16:09:19 +0500

    updated code with changed variable name

--- app.py
+++ app.py
@@ -3,10 +3,10 @@ import gradio as gr
 def greet(name, intensity):
     return "Hello, " + name + "!" * int(intensity)
 
-demo = gr.Interface(
+demo1 = gr.Interface(
     fn=greet,
     inputs=["text", "slider"],
     outputs=["text"],
 )
 
-demo.launch()
+demo1.launch()
